ac0f071694866c61bdb31ffffdb4ea3d1efd9b7e
DevsJeanPC <jean.ingsoftware@gmail.com>
2024-08-16 12:18:16 -0600

    Dia 3 parte 1

---
+++ pythonProject/Dia 3/Metodos.py
@@ -0,0 +1,14 @@
+#upper mayus, lower min, split separar en lista, find es como index pero no arroja error si no se encuentra algo.
+
+texto = "Este es el texto de Jean"
+
+resultado = texto.replace("Jean","todos")
+
+print(resultado)
+
+a = "Aprender"
+b = "Python"
+c = "es"
+d = "genial"
+e = " ".join([a,b,c,d])
+print(e)
