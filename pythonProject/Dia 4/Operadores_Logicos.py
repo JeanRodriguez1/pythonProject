9cb5e8ac449bc6f3c678f316c64c6e4d467606fb
DevsJeanPC <jean.ingsoftware@gmail.com>
2024-09-02 12:58:51 -0600

    Operadores Logicos

---
+++ pythonProject/Dia 4/Operadores_Logicos.py
@@ -0,0 +1,18 @@
+mi_bool = (4 < 5) and (5 == 2+3)
+print(mi_bool)
+
+mi_bool2 = (55 == 55) and ('perro' == 'perro')
+print(mi_bool2)
+# En or le da los mismo mientras una sea verdadera
+mi_bool3 = (1 == 10) or (3 == 3)
+print(mi_bool3)
+
+texto = "Esta frase es breve"
+mi_bool4 = ('frase' in texto) and ('python' in texto)
+print(mi_bool4)
+
+mi_bool5 = not ('a' == 'a')
+print(mi_bool5)
+
+
+mi_bool = palabra1 not in frase and palabra2 not in frase
