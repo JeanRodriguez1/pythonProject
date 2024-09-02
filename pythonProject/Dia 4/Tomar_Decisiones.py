d235fc5716868c323f483a2a09955d9c0ca2a889
DevsJeanPC <jean.ingsoftware@gmail.com>
2024-09-02 13:31:55 -0600

    Control de flujo !

---
+++ pythonProject/Dia 4/Tomar_Decisiones.py
@@ -0,0 +1,24 @@
+"""mascota = 'perro'
+
+if mascota == 'gato':
+    print('Tienes un gato')
+elif mascota == 'perro':
+    print('Tienes un perro')
+elif mascota == 'pez':
+    print('Tienes un pez')
+else:
+    print("No se que animal tienes")"""
+
+# Otras formas
+
+edad = 16
+calificacion = 6
+
+if edad < 18 :
+    print('Eres menor de edad')
+    if calificacion >= 7:
+        print('Aprobado')
+    else :
+        print('No Aprobado')
+else:
+    print('Eres un adulto')
