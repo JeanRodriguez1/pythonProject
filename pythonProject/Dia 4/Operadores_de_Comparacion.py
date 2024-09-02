87b5384bd1702ef48c31bb46d93892f5ed518434
DevsJeanPC <jean.ingsoftware@gmail.com>
2024-09-02 12:39:22 -0600

    Operadores de comparacion

---
+++ pythonProject/Dia 4/Operadores_de_Comparacion.py
@@ -0,0 +1,9 @@
+mi_bool = 5+5==18-8
+print(mi_bool)
+# lo importante es el resultado y no el como se logro el resultado
+mi_bool2 = 'blanco' == 'Blanco'.lower()
+print(mi_bool2)
+
+mi_bool3 = 100.0== 100
+print(mi_bool3)
+
