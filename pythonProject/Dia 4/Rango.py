340bcb514228348916a2f14932c7904f9f5cda40
DevsJeanPC <jean.ingsoftware@gmail.com>
2024-09-03 11:02:31 -0600

    Rango

---
+++ pythonProject/Dia 4/Rango.py
@@ -0,0 +1,27 @@
+""" Ejemplo 1
+for num in range(0,6,2):
+    print(num)
+
+"""
+""" Ejemplo 2 
+lista = list(range(1,101))
+print(lista)
+"""
+
+""" Practicas 
+
+mi_lista = list(range(2500,2586))
+
+mi_lista = list(range(3,301,3))
+
+
+suma_cuadrados = 0
+
+
+for i in range(1,16):
+    suma_cuadrados += i**2
+
+
+
+
+"""
