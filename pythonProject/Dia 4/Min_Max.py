651a8a72fb909b9360d0d46ae90ed9781e067c84
DevsJeanPC <jean.ingsoftware@gmail.com>
2024-09-05 16:55:06 -0600

    Min y max

---
+++ pythonProject/Dia 4/Min_Max.py
@@ -0,0 +1,17 @@
+"""
+menor = min(58,96,73,45,23)
+mayor = max(58,96,73,45,23)
+print(mayor)
+
+"""
+
+"""
+lista = [58,96,73,45,23]
+print(f'El menor es {min(lista)} y el mayor es {max(lista)}')
+"""
+nombre = ['jean','liver','pablo','carlos']
+uno = "Carlos" #Imprime C por que primero da las mayusculas. utiliza .lower si hay variacion de letras.
+print(min(nombre))
+
+dic = {'C1':45,'C2':11}
+print(min(dic.values()))
