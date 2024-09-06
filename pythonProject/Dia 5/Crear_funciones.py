4153da9bb49691da6ec31b5a1ec0f3958abbe062
DevsJeanPC <jean.ingsoftware@gmail.com>
2024-09-06 15:55:33 -0600

    Crear funciones

---
+++ pythonProject/Dia 5/Crear_funciones.py
@@ -0,0 +1,20 @@
+def saludar_persona(nombre): # Esta funcion sirve para saludar a las personas.
+    print("Hola " + nombre)
+
+saludar_persona("Jean")
+
+# Práctica Crear Funciones 1
+def saludar():
+    print("¡Hola mundo!")
+
+# Práctica Crear Funciones 2
+nombre_persona = "Jean"
+
+def bienvenida(nombre_persona):
+    print(f"¡Bienvenido {nombre_persona}!")
+
+# Práctica Crear Funciones 3
+un_numero = 2
+
+def cuadrado(num):
+    print(num**2)
