a7efaf33836aa1ebe8933c91c44f170789e7bcb9
DevsJeanPC <jean.ingsoftware@gmail.com>
2024-09-11 08:00:08 -0600

    *args practicas

--- pythonProject/Dia 5/Args.py
+++ pythonProject/Dia 5/Args.py
@@ -12,3 +12,27 @@ def sumapro(*args): # lo importante es el * es buena practica utilizar args
     return total # se puede utilizar sum(args) y da el mismo resultado de funcion
 
 print(sumapro(5,6,7,6,8,9)) # son funciones que limitan argumentos
+
+
+
+
+# Práctica sobre Argumentos Indefinidos (*args) 1
+def suma_cuadrados(*args):
+    suma = 0
+    for arg in args:
+        suma += arg ** 2
+
+    return suma
+
+# Práctica sobre Argumentos Indefinidos (*args) 2
+def suma_absolutos(*args):
+    suma = 0
+    for arg in args:
+        suma += abs(arg)
+
+    return suma
+
+# Práctica sobre Argumentos Indefinidos (*args) 3
+def numeros_persona(nombre, *args):
+    suma_numeros = sum(args)
+    return f'{nombre}, la suma de tus números es {suma_numeros}'
