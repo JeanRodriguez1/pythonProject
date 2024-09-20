6155b404db347a7756a793fbddf080b674b0e778
DevsJeanPC <jean.ingsoftware@gmail.com>
2024-09-20 10:15:39 -0600

    Dia 7 Programación Orientado a Objetos Clases

---
+++ pythonProject/Dia 7/Clases.py
@@ -0,0 +1,44 @@
+class Pajaro:
+    pass
+
+
+mi_pajaro = Pajaro()
+otro_pajaro = Pajaro()
+
+print(mi_pajaro)
+print(otro_pajaro)
+print(type(mi_pajaro))
+
+
+""" PRACTICAS
+
+# Práctica Clases 1
+
+class Personaje:
+    pass
+
+harry_potter = Personaje()
+
+
+# Práctica Clases 2
+
+class Dinosaurio:
+    pass
+
+velociraptor = Dinosaurio()
+tiranosaurio_rex = Dinosaurio()
+braquiosaurio = Dinosaurio()
+
+
+# Práctica Clases 3
+
+class PlataformaStreaming:
+    pass
+
+
+netflix = PlataformaStreaming()
+hbo_max = PlataformaStreaming()
+amazon_prime_video = PlataformaStreaming()
+
+
+"""
