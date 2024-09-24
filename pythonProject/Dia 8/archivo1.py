d3eb7d9a4827725826700c9257786611b1c12bbd
DevsJeanPC <jean.ingsoftware@gmail.com>
2024-09-24 12:00:01 -0600

    pylint

---
+++ pythonProject/Dia 8/archivo1.py
@@ -0,0 +1,17 @@
+"""
+Archivo con error intencional para pylint
+Pylint es un verificador de código, errores y calidad para
+Python, siguiendo el estilo recomendado por PEP 8, la guía de
+estilo de Python. Es de gran utilidad en el trabajo en equipo
+
+en la ruta en comando :
+pylint archivo1.py -r y
+"""
+
+
+def una_funcion():
+    numero1 = 500
+    print(numero1)
+
+
+una_funcion()
