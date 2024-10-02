785cf9837310553acf3817304ac9761583cb0b82
devsjeanmac <jean.ingsoftware@gmail.com>
2024-10-02 13:07:59 -0600

    import math

---
+++ pythonProject/Dia 9/modulo_math.py
@@ -0,0 +1,41 @@
+import math
+
+resultado = math.floor(89.778) # redondeo hacia abajo
+resultado1 = math.ceil(89.778) # redondeo hacia arriba
+resultado2 = math.pi # raiz cuadrada
+resultado3 = math.log(25,5)
+resultado4 = math.tan(2565) # tangente
+resultado5 = math.cos(2345) # coseno
+
+
+print(resultado)
+
+"""
+
+# Práctica Módulo Math 1
+
+import math
+ 
+resultado = math.log10(25)
+
+# Práctica Módulo Math 2
+
+import math
+ 
+resultado = math.sqrt(math.pi)
+
+
+# Práctica Módulo Math 3
+
+import math
+ 
+resultado = math.factorial(7)
+
+
+
+
+
+
+
+
+"""
