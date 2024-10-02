7fd11d1705dd4a9d235334014d2837ef22c87a13
devsjeanmac <jean.ingsoftware@gmail.com>
2024-10-02 12:27:57 -0600

    os y shutil

---
+++ pythonProject/Dia 9/os_shutil.py
@@ -0,0 +1,21 @@
+import os, shutil
+print(os.getcwd()) # conocer directorio donde se esta trabajando.
+
+"""
+# crear archivo
+archivo = open('curso.txt', 'w')
+archivo.write('texto de prueba')
+archivo.close()
+
+"""
+# mover archivo
+# shutil.move('curso.txt',"ruta a donde quieres")
+
+"""
+# Eliminar archivos
+Descargar desde terminal -  pip intall send2trash
+importar send2trash
+send2trash.send2trash('archivo.txt')
+"""
+# recorrertodo en la ruta os.walk estudiar bien
+
