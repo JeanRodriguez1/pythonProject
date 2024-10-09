448c66183da7acdb186dd799130bd5fef406431b
devsjeanmac <jean.ingsoftware@gmail.com>
2024-10-09 11:13:23 -0600

    tkinder - ventana

--- pythonProject/Dia 12/mi_restaurante.py
+++ pythonProject/Dia 12/mi_restaurante.py
@@ -1,1 +1,16 @@
 from tkinter import *
+
+# iniciar tkinter
+aplicacion = Tk()
+
+# tamaño de la ventana
+aplicacion.geometry('1020x630+0+0')
+
+# evitar maximizar ventana
+aplicacion.resizable(0, 0)
+
+# titulo de la ventana
+aplicacion.title("Mi Restaurante - Sistema de Facturacion")
+
+# color de fondo de la ventana
+aplicacion.config(bg='burlywood')
