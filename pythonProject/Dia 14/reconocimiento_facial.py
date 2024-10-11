fd154383e988c6fda225c264dba00e5db0ef4ea0
devsjeanmac <jean.ingsoftware@gmail.com>
2024-10-11 10:59:22 -0600

    Dia 14 proyecto

---
+++ pythonProject/Dia 14/reconocimiento_facial.py
@@ -0,0 +1,9 @@
+"""
+La practica no la pude realizar ya que no cuento con webcam.
+Se tiene que descargar visual estudio con complemento c++.
+Biblioteca - cmake.
+Biblioteca - dlib
+Biblioteca - face-recognition
+Biblioteca - numpy
+Biblioteca - opencv-python
+"""
