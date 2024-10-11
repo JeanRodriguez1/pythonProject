661cfe824ea7344a748465f835e68c8a40e8be0d
devsjeanmac <jean.ingsoftware@gmail.com>
2024-10-11 11:03:12 -0600

    crear base de datos

--- pythonProject/Dia 14/reconocimiento_facial.py
+++ pythonProject/Dia 14/reconocimiento_facial.py
@@ -12,3 +12,16 @@ import face_recognition as fr
 import os
 import numpy
 from datetime import datetime
+
+# crear base de datos
+ruta = 'Empleados'
+mis_imagenes = []
+nombres_empleados = []
+lista_empleados = os.listdir(ruta)
+
+for nombre in lista_empleados:
+    imagen_actual = cv2.imread(f'{ruta}/{nombre}')
+    mis_imagenes.append(imagen_actual)
+    nombres_empleados.append(os.path.splitext(nombre)[0])
+
+print(nombres_empleados)
