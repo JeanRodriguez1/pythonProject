ccf4e4f74445590a8b80537ed52e8be47d7a53f0
devsjeanmac <jean.ingsoftware@gmail.com>
2024-10-21 15:38:14 -0600

    Programa una aplicacion web de tareas pendientes

--- pythonProject/Dia 16/pythonWeb/src/proyecto/base/urls.py
+++
@@ -1,5 +0,0 @@
-from django.urls import path
-from .views import ListaPendientes, DetalleTarea
-
-urlpatterns = [path('', ListaPendientes.as_view(), name='pendientes'),
-               path('tarea/<int:pk>', DetalleTarea.as_view(), name='tarea')]
